4a4adddf0686ef2bc5de0cf1080baefa1dbc6889
Philip Edwards <philip@gdcorner.com>
2015-09-06 20:02:52 +1000

    Added first versions of FileRecord and VersionRecord headers

---
+++ src/VersionRecord.h
@@ -0,0 +1,15 @@
+#include <string>
+class FileRecord
+{
+public:
+	
+protected:
+	//The version number of this version
+	int VersionNumber;
+	//the length of this version in bytes
+	int Length;
+	//the has of the entire version of the file
+	int OverallHash;
+private:
+
+};
