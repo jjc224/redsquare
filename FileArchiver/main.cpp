d2dc80f214dfec36dc4244bd49cd817ebd80a01a
philipedwards <philipedwards@devops2.local.gdcorner.com>
2015-09-16 11:54:38 +1000

    Added a blank netbeans project

---
+++ FileArchiver/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   main.cpp
+ * Author: philipedwards
+ *
+ * Created on 16 September 2015, 11:53 AM
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
