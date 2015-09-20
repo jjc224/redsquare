e34daf38377ec38779e9e95f5a7460e2254def3a
Philip Edwards <philip@gdcorner.com>
2015-09-20 16:42:31 +1000

    Added first test code to push a file to the database

--- FileArchiver/main.cpp
+++ FileArchiver/main.cpp
@@ -6,12 +6,25 @@
  */
 
 #include <QApplication>
+#include "ProjectConstants.h"
+#include "FileRecord.h"
+
+#include <string>
+using namespace std;
 
 int main(int argc, char *argv[]) {
 	// initialize resources, if needed
 	// Q_INIT_RESOURCE(resfile);
 
 	QApplication app(argc, argv);
+	
+	FileRecord newFile;
+	bool bSuccess = newFile.CreateFile("MurmurHash3.cpp");
+	
+	string logMessage = "The result was: ";
+	logMessage += bSuccess;
+	
+	log(logMessage);
 
 	// create and show your widgets here
 
