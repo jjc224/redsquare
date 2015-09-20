332dccd10c5cd6d4eafffd47d59c32eac2cc0ff8
ThomasNixon <cant.get.nixon@gmail.com>
2015-09-20 19:21:21 +1000

    Update db.sql
    Added auto increment and delete tables for easy clean up

--- FileArchiver/sql/db.sql
+++ FileArchiver/sql/db.sql
@@ -1,5 +1,5 @@
 create table File (
-	filename varchar(767),
+	filename varchar(767) not null,
 	curhash int(11),
 	curversion int(11),
 	numversions int(11),
@@ -7,7 +7,7 @@ create table File (
 );
 
 create table Version (
-	id int(11),
+	id int(11) not null auto_increment,
 	filename varchar(255),
 	size int(11),
 	time int(11),
@@ -19,17 +19,9 @@ create table Version (
 	foreign key (filename) references File(filename)
 );
 
-create table VtoB (
-	versionid int(11),
-	blockid int(11),
-	versionindex int(11),
-	primary key (versionid, blockid),
-	foreign key (versionid) references Version(id),
-	foreign key (blockid) references Block(id)
-);
 
 create table Block (
-	id int(11),
+	id int(11) not null auto_increment,
 	hash1 int(11),
 	hash2 int(11),
 	data mediumblob,
@@ -37,3 +29,32 @@ create table Block (
 	index(hash1),
 	index(hash2)
 );
+
+
+create table VtoB (
+	id int(11) not null auto_increment,
+	versionid int(11),
+	blockid int(11),
+	versionindex int(11),
+	primary key (id),
+	foreign key (versionid) references Version(id),
+	foreign key (blockid) references Block(id)
+);
+
+alter table Version auto_increment = 1;
+alter table Block auto_increment = 1;
+alter table VtoB auto_increment = 1;
+
+
+delete from VtoB;
+delete from Block;
+delete from Version;
+delete from File;
+
+
+drop table VtoB;
+drop table Block;
+drop table Version;
+drop table File;
+
+
