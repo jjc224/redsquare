bb51db1be64c106c0eee297cfd715441fc63b4f1
Philip Edwards <philip@gdcorner.com>
2015-09-23 12:08:07 +1000

    Updated db schema

--- FileArchiver/sql/db.sql
+++ FileArchiver/sql/db.sql
@@ -1,27 +1,27 @@
 create table File (
 	filename varchar(767) not null,
-	curhash int(11),
-	curversion int(11),
-	numversions int(11),
+	curhash int(11) unsigned,
+	curversion int(11) unsigned,
+	numversions int(11) unsigned,
 	primary key (filename)
 );
 
 create table Version (
-	id int(11) not null auto_increment,
+	id int(11) unsigned not null auto_increment,
 	filename varchar(767),
-	size int(11),
-	time int(11),
-	filemodtime int(11),
+	size int(11) unsigned,
+	time int(11) unsigned,
+	filemodtime int(11) unsigned,
 	comment mediumtext,
-	version int(11),
-	hash int(11),
+	version int(11) unsigned,
+	hash int(11) unsigned,
 	primary key (id),
 	foreign key (filename) references File(filename)
 );
 
 
 create table Block (
-	id int(11) not null auto_increment,
+	id int(11) unsigned not null auto_increment,
 	hash1 int(11) unsigned,
 	hash2 int(11) unsigned,
 	data mediumblob,
@@ -33,9 +33,9 @@ create table Block (
 
 create table VtoB (
 	id int(11) not null auto_increment,
-	versionid int(11),
-	blockid int(11),
-	versionindex int(11),
+	versionid int(11) unsigned,
+	blockid int(11) unsigned,
+	versionindex int(11) unsigned,
 	primary key (id),
 	foreign key (versionid) references Version(id),
 	foreign key (blockid) references Block(id)
